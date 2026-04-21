eb38d894e5454578ad029c2ce45fc6dfc01d7a8a
DaniAThaheem <danimsbzuacc@outlook.com>
2026-04-21 21:24:47 +0500

    Modified java version and configured the project

--- android/app/src/main/java/com/example/startup_lense/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.startup_lense;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/startup_lense/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.startup_lense
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
